31e46179c6a02fd9ab1c612f7320ae8981d9bc6c
pankaj kekane <84341027+pankajkekane@users.noreply.github.com>
2021-06-25 09:28:11 +0000

    css modifications

--- all files/index.js
+++
@@ -1,9 +0,0 @@
- const hamburgermenu = document.getElementById("hamburgermenu");
-      const mobileNavDropDown = document.querySelector(
-        ".mobile__navbar__items"
-      );
-      window.addEventListener("click", () => {
-        if (mobileNavDropDown.classList.contains("hamburger__menu__close"))
-          return mobileNavDropDown.classList.remove("hamburger__menu__close");
-        return mobileNavDropDown.classList.add("hamburger__menu__close");
-      });
